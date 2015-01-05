a059f4180e9220d4ea0ebff9c218f93efbd24306
Amy Boyd <amy@amyboyd.co.uk>
2015-01-05 13:04:58 -0500

    Import my bash aliases from GitHub.

--- manifests/site.pp
+++ manifests/site.pp
@@ -5,3 +5,8 @@ Exec {
 include git
 git::repo { 'nicesky': }
 git::repo { 'server-anastasia-puppet': }
+
+exec { 'create bash_aliases':
+    command => 'wget -nc https://raw.githubusercontent.com/amyboyd/amybits/master/.bash_profile_for_linux -O /root/.bash_aliases',
+    creates => '/root/.bash_aliases',
+}
