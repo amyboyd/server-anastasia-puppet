4d140fdd4b77094479174dbcd5ab286a01499a21
Amy Boyd <amy@amyboyd.co.uk>
2015-09-13 11:59:15 +0100

    Add amy.boyd and bernardo.silva to the 'developers' group.

--- modules/developers/manifests/init.pp
+++ modules/developers/manifests/init.pp
@@ -3,8 +3,13 @@ class developers {
         ensure => present,
     }
 
-    developers::developer { 'bernardo.silva': }
-    developers::developer { 'amy.boyd': }
+    developers::developer { 'bernardo.silva':
+        groups => ['developers'],
+    }
+
+    developers::developer { 'amy.boyd':
+        groups => ['developers'],
+    }
 
     include developers::sudo_rights
 }
